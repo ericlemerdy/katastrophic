f5b677c471c20496ff2facb3c9251a91cfc32212
Éric Le Merdy <eric.lemerdy@laposte.net>
2014-03-22 09:23:59 +0100

    Full coverage

---
+++ java/src/test/java/name/lemerdy/eric/Inv3st_PlanTest.java
@@ -0,0 +1,18 @@
+package name.lemerdy.eric;
+
+import org.junit.Test;
+
+import static name.lemerdy.eric.Inv3st_Plan.input;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class Inv3st_PlanTest {
+    @Test
+    public void should_start() {
+        assertThat(input("42 " +
+                "42 " +
+                "1 2 3 4 5 6 7 8 9 10 11 12 " +
+                "3 " +
+                "12 11 10 9 8 7 6 5 4 3 2 1 ").output()).isEqualTo("Case #1: 1 12 462\n" +
+                "Case #2: IMPOSSIBLE");
+    }
+}
