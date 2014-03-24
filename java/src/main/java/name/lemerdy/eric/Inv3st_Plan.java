9aad3eae103ca047e2fbf871a76042d4b34b7619
Éric Le Merdy <eric.lemerdy@laposte.net>
2014-03-24 05:49:54 +0100

    Refactoring : rename variables to finally understand what it does.

--- java/src/main/java/name/lemerdy/eric/Inv3st_Plan.java
+++ java/src/main/java/name/lemerdy/eric/Inv3st_Plan.java
@@ -18,7 +18,7 @@ public class Inv3st_Plan {
     public String run(String input) {
         Scanner scanner = new Scanner(input);
         scanner.nextInt();
-        List<String> allowingPrfts1 = new ArrayList<>();
+        List<String> profits = new ArrayList<>();
         int caseCount = 0;
         while (scanner.hasNext()) {
             caseCount++;
@@ -27,30 +27,28 @@ public class Inv3st_Plan {
             for (int i = 1; i <= 12; i++) {
                 mList.add(scanner.nextInt());
             }
-            int best_bM = MIN_VALUE;
-            int best_sM = MIN_VALUE;
-            int limitPrft = MIN_VALUE;
+            int bestBuyMonth = 0;
+            int bestSellMonth = 0;
+            int maxProfit = 0;
             for (int i = 1; i <= 12; i++) {
                 for (int j = i; j <= 12; j++) {
-                    int sellPrice = mList.get(i - 1);
-                    int quantity = amount / sellPrice;
-                    int the_bal = -quantity * sellPrice;
-                    int prce_buy = mList.get(j - 1);
-                    int rev = quantity * prce_buy;
-                    the_bal += rev;
-                    if (the_bal > limitPrft) {
-                        limitPrft = the_bal;
-                        best_bM = i;
-                        best_sM = j;
+                    int buyPrice = mList.get(i - 1);
+                    int sellPrice = mList.get(j - 1);
+                    int quantity = amount / buyPrice;
+                    int profit = - (quantity * buyPrice) + (quantity * sellPrice);
+                    if (profit > maxProfit) {
+                        maxProfit = profit;
+                        bestBuyMonth = i;
+                        bestSellMonth = j;
                     }
                 }
             }
-            if (limitPrft <= 0) {
-                allowingPrfts1.add(format("Case #%d: IMPOSSIBLE", caseCount));
+            if (maxProfit == 0) {
+                profits.add(format("Case #%d: IMPOSSIBLE", caseCount));
             } else {
-                allowingPrfts1.add(format("Case #%d: %d %d %d", caseCount, best_bM, best_sM, limitPrft));
+                profits.add(format("Case #%d: %d %d %d", caseCount, bestBuyMonth, bestSellMonth, maxProfit));
             }
         }
-        return on('\n').join(allowingPrfts1);
+        return on('\n').join(profits);
     }
 }
