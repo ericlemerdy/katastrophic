cb9c261626f975dc12e42a287b33ea64148412e4
Éric Le Merdy <eric.lemerdy@laposte.net>
2014-03-24 05:24:57 +0100

    Refactoring : change loop boundaries

--- java/src/main/java/name/lemerdy/eric/Inv3st_Plan.java
+++ java/src/main/java/name/lemerdy/eric/Inv3st_Plan.java
@@ -31,19 +31,17 @@ public class Inv3st_Plan {
             int best_sM = MIN_VALUE;
             int limitPrft = MIN_VALUE;
             for (int i = 1; i <= 12; i++) {
-                for (int j = 1; j <= 12; j++) {
-                    if (i < j) {
-                        int sellPrice = mList.get(i - 1);
-                        int quantity = amount / sellPrice;
-                        int the_bal = -quantity * sellPrice;
-                        int prce_buy = mList.get(j - 1);
-                        int rev = quantity * prce_buy;
-                        the_bal += rev;
-                        if (the_bal > limitPrft) {
-                            limitPrft = the_bal;
-                            best_bM = i;
-                            best_sM = j;
-                        }
+                for (int j = i; j <= 12; j++) {
+                    int sellPrice = mList.get(i - 1);
+                    int quantity = amount / sellPrice;
+                    int the_bal = -quantity * sellPrice;
+                    int prce_buy = mList.get(j - 1);
+                    int rev = quantity * prce_buy;
+                    the_bal += rev;
+                    if (the_bal > limitPrft) {
+                        limitPrft = the_bal;
+                        best_bM = i;
+                        best_sM = j;
                     }
                 }
             }
